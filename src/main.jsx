9480ab21651dd3addd383245b78ba1b84a2a4360
theodesde <theo.desde@gmail.com>
2025-11-28 18:31:39 +0100

    Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// IMPORT DE VERCEL ANALYTICS
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+    {/* AJOUT DU COMPOSANT ANALYTICS ICI */}
+    <Analytics />
+  </React.StrictMode>,
+)
