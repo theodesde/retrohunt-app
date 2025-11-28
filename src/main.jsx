5ca819ef47503aa4f5e954b661f3f61358a7c41b
theodesde <theo.desde@gmail.com>
2025-11-28 19:08:10 +0100

    Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// IMPORT DE VERCEL ANALYTICS
+
+// Import des outils Vercel
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* Tracking des visiteurs */}
     <Analytics />
+    {/* Tracking de la performance (Vitesse) */}
+    <SpeedInsights />
   </React.StrictMode>,
 )
